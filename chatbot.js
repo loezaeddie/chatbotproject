4a70256bc42bc16505b1bd950ad99a1eccfcfe48
Eddie M. Loeza <loezaeddie@gmail.com>
2016-09-23 11:19:35 -0700

    Response
    My Trump Bot Responds back

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,9 @@
 function askQuestion() {
   var input = document.getElementById("input").value;
-  document.getElementById("chat-area").textContent += input;
+  var giveresponse = responses[input]
+  document.getElementById("chat-area").textContent += input + ": " + giveresponse + " ";
+}
+
+var responses = {
+  "What do you think about Hillary" : "Hillary Clinton invented ISIS with her stupid policies. She is responsible for ISIS.— Donald Trump on Sunday, July 17th, 2016 "
 }
