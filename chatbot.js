aa89b991cc4b0a565dfce3205ae81c130b281004
Eddie M. Loeza <loezaeddie@gmail.com>
2016-09-30 14:59:33 -0700

    Time
    I added the code for the time

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,6 @@
+var d = new Date();
+
+
 function askQuestion() {
   var input = document.getElementById("input").value;
   input = input.toLowerCase()
@@ -20,9 +23,11 @@ var responses = {
 
 "how old are you?" : "I am currently 70 years old<br/> <br/>",
 
+"what time is it?" : "It is <br/>" + d.toLocaleTimeString(),
+
 "what are your plans for the border?" : "I will build a great, great wall on our southern border. And I will have Mexico pay for that wall.- July 27th, 2016<br/> <br/>",
 
 "hi" : "Yes my wall will be very high.<br/> <br/>",
 
-"how are you?" : "I'm doing well.<br/> <br/>"
+"how are you?" : "I'm doing well.<br/> <br/>",
 }
